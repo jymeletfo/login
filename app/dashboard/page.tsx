d58f24882b8036a29e13ca2a06d19466097ba695
TFO\jymele <jymele@tfo.org>
2025-05-13 17:09:29 -0400

    Refactor layout and dashboard components: remove unused imports and simplify body class

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { signOut, auth } from "@/auth";
+import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import Navbar from "@/custom/navbar";
 
